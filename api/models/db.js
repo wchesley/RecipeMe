908341209e8de217dafc6a710d0ef8a7ba557080
Walker Chesley <chesley.walker@gmail.com>
2019-11-26 14:27:09 -0600

    init commit

---
+++ api/models/db.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+let dbURI = process.env.MONGO_DB_URI; 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .catch(err => console.log(`ERROR CONNECTING TO DATABASE: ${err}`));
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to MongoDB`);
+});
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error:', err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+// For nodemon restarts                                 
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
+});
+
+require('./users');
