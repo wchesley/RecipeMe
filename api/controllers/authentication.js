1277eab492dc2f16b65117058c6a27e8545a5673
Walker Chesley <chesley.walker@gmail.com>
2019-11-27 09:31:16 -0600

    added minor error handling
    updated to es6 syntax

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,50 +1,59 @@
-var passport = require('passport');
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
+let passport = require('passport');
+let mongoose = require('mongoose');
+let User = mongoose.model('User');
 
-var sendJSONresponse = function(res, status, content) {
+let sendJSONresponse = (res, status, content) => {
   res.status(status);
   res.json(content);
 };
 
-module.exports.register = function(req, res) {
+module.exports.register = (req, res) => {
 
-  // if(!req.body.name || !req.body.email || !req.body.password) {
-  //   sendJSONresponse(res, 400, {
-  //     "message": "All fields required"
-  //   });
-  //   return;
-  // }
+  if (!req.body.name || !req.body.email || !req.body.password) {
+    sendJSONresponse(res, 400, {
+      "message": "All fields required"
+    });
+    return;
+  }
 
-  var user = new User();
+  let user = new User();
 
   user.name = req.body.name;
   user.email = req.body.email;
 
   user.setPassword(req.body.password);
 
-  user.save(function(err) {
-    var token;
-    token = user.generateJwt();
-    res.status(200);
-    res.json({
-      "token" : token
-    });
+  user.save((err) => {
+    if (err) {
+      res.status(403);
+      res.json({
+        'err': 'Error creating user'
+      });
+      return;
+    }
+    else {
+      let token;
+      token = user.generateJwt();
+      res.status(200);
+      res.json({
+        "token": token
+      })
+    };
   });
 
 };
 
-module.exports.login = function(req, res) {
+module.exports.login = (req, res) => {
 
-  // if(!req.body.email || !req.body.password) {
-  //   sendJSONresponse(res, 400, {
-  //     "message": "All fields required"
-  //   });
-  //   return;
-  // }
+  if (!req.body.email || !req.body.password) {
+    sendJSONresponse(res, 400, {
+      "message": "All fields required"
+    });
+    return;
+  }
 
-  passport.authenticate('local', function(err, user, info){
-    var token;
+  passport.authenticate('local', (err, user, info) => {
+    let token;
 
     // If Passport throws/catches an error
     if (err) {
@@ -53,11 +62,11 @@ module.exports.login = function(req, res) {
     }
 
     // If a user is found
-    if(user){
+    if (user) {
       token = user.generateJwt();
       res.status(200);
       res.json({
-        "token" : token
+        "token": token
       });
     } else {
       // If user is not found
