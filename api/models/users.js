12cf335d0918a79eb0ce6851368e3ac335d349df
Walker Chesley <chesley.walker@gmail.com>
2019-11-27 09:30:35 -0600

    moved jwt secret to env var

--- api/models/users.js
+++ api/models/users.js
@@ -35,7 +35,7 @@ userSchema.methods.generateJwt = function() {
     email: this.email,
     name: this.name,
     exp: parseInt(expiry.getTime() / 1000),
-  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+  }, process.env.JWT_SECRET); 
 };
 
 mongoose.model('User', userSchema);
