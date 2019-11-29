dc7307f21003dc7f48d7f44d233a29a368c2e12c
Walker Chesley <chesley.walker@gmail.com>
2019-11-29 11:07:05 -0600

    cleaned up profile

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,28 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService, UserDetails } from '../authentication.service';
-import { RecipeAPIService } from '../recipe-api.service';
 
 @Component({
   templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
   details: UserDetails;
-  recipes;
-  recipeTitle;
 
-  constructor(private auth: AuthenticationService, private recipe: RecipeAPIService) {}
+  constructor(private auth: AuthenticationService) {}
   
   ngOnInit() {
-    this.recipe.getRecipe().subscribe( data => {
-      // console.log(data);
-      let food = data["meals"];
-      let f = food[0];
-      console.log(f);
-      console.log(f["strMeal"])
-      this.recipeTitle = f["strMeal"];
-      this.recipes = f;
 
-    })
     this.auth.profile().subscribe(user => {
       this.details = user;
     }, (err) => {
