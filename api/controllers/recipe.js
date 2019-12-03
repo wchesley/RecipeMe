ebe86515aea363bf884fa9e3c5ecb6af70dd4fb5
Walker Chesley <chesley.walker@gmail.com>
2019-12-03 10:28:10 -0600

    attempts made at saving recipe's ID to user
    bugged atm, won't save recipeList to existing users, but will create field for it for new users (those created after I made this change)
    Currently writes recipe to database tho, so theres that

--- api/controllers/recipe.js
+++ api/controllers/recipe.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 let recipe = mongoose.model('Recipe');
+let user = mongoose.model('User');
 
 let saveRecipe = (req, res, next) => {
     let Recipe = new recipe;
@@ -7,16 +8,24 @@ let saveRecipe = (req, res, next) => {
     Recipe.instructions  = req.body.instructions; 
     Recipe.category = req.body.category;
     Recipe.cuisine = req.body.cuisine;
-    Recipe.ingredients = req.body.ingredients;
+    //Recipe.ingredients = req.body.ingredients;
     Recipe.imgURL = req.body.imgURL;
-    Recipe.save((err) => {
+    Recipe.save((err, savedObj) => {
         if(err){
             console.log(err);
             res.status(403).send({error: "error saving to database"});
             return;
         }
         else{
-            console.log("POST /api/saverecipe");
+            objref = savedObj.id;
+            console.log("SAVED:"+objref);
+            user.findOneAndUpdate(
+                {email: Recipe.user},
+                {$push: {recipesList:objref}}, (err) =>{
+                    if(err){
+                        console.log(err);
+                    }
+                })
             res.status(200).json(Recipe);
             return; 
         }
