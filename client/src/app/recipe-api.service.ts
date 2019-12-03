ec75e8fb02c1c480a72e26749fd904f696d5a700
Walker Chesley <chesley.walker@gmail.com>
2019-12-03 10:31:02 -0600

    fixed formatting

--- client/src/app/recipe-api.service.ts
+++ client/src/app/recipe-api.service.ts
@@ -5,19 +5,20 @@ import { Observable } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
-  providedIn:"root"
+  providedIn: "root"
 })
 
 export class RecipeAPIService {
 
   constructor(private httpClient: HttpClient, private auth: AuthenticationService) { }
-  public getRecipe(query:String) {
-    return this.httpClient.get('https://www.themealdb.com/api/json/v1/1/search.php?s='+query).pipe(
+
+  public getRecipe(query: String) {
+    return this.httpClient.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + query).pipe(
       map(res => res)
     );
   }
 
-  public saveRecipe(recipe:any): Observable<any> {
+  public saveRecipe(recipe: any): Observable<any> {
     return this.httpClient.post('/api/saverecipe', recipe);
   }
 }
