4a5248eb10e3f4763a270edeef7daa990731b287
Walker Chesley <chesley.walker@gmail.com>
2019-12-03 08:35:01 -0600

    removed extra junk from api service

--- client/src/app/recipe-api.service.ts
+++ client/src/app/recipe-api.service.ts
@@ -9,14 +9,8 @@ import { AuthenticationService } from './authentication.service';
 })
 
 export class RecipeAPIService {
-private token:string;
+
   constructor(private httpClient: HttpClient, private auth: AuthenticationService) { }
-  private getToken(): string {
-    if (!this.token) {
-      this.token = localStorage.getItem('mean-token');
-    }
-    return this.token;
-  }
   public getRecipe(query:String) {
     return this.httpClient.get('https://www.themealdb.com/api/json/v1/1/search.php?s='+query).pipe(
       map(res => res)
@@ -24,7 +18,6 @@ private token:string;
   }
 
   public saveRecipe(recipe:any): Observable<any> {
-    this.token = localStorage.getItem('mean-token')
     return this.httpClient.post('/api/saverecipe', recipe);
   }
 }
