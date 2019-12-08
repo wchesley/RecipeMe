ef9e06368b214758c713c4a9dbf00478064f965d
Walker Chesley <chesley.walker@gmail.com>
2019-12-07 20:31:49 -0600

    reworked backend recipe
    reworked code to where it actually works now ;)
    ref lines 8-26, found working solution in reddit of all places, crawled stackoverflow and google for a good while, visited many blogs and finally wound up here: https://www.reddit.com/r/learnjavascript/comments/55mjr7/mongoose_and_inserting_subdocuments_to_a_user/
       Specifically top comment: "First you need to User.findOne({_id:req.body.id}, function(err,doc){ If(err)res.sendStatus(500).send('database error' ).end(); If(!doc){res.sendStatus(404).send('user was not found').end() }else{ doc.logsHeld.push({logName: req.body.logNameInput}); doc.markModified('logsHeld') doc.save(); res.sendStatus(200).send('log saved').end(); } })
    
    First you have to pull a user from the database. Once Mongoose returns the document from the "findOne" method you can modify the document. You need to tell the database hey I modified something make sure you save it. Which is why mongoose has the portion to 'markModified' then you have to send it to the database to be saved. So you end with doc.save().
    
    But you also need to respond to the client or user or whomever by sending a A Okay and ideally some message so that a notification system on the front end can signal the user that it happened. Or you can return other stuff. Or you can go to the next middleware. Or you can or you can or you can............ Do anything your heart desires wooo programming.
    
    Also sorry for formatting on my phone and it can never get the code right."
    
    updated getSavedRecipes...guess technically I am just reading the user here (so this is duplicate code), especially since I switched up the recipe sub schema to save the whole recipe obj to the user's doc.

--- api/controllers/recipe.js
+++ api/controllers/recipe.js
@@ -1,41 +1,64 @@
 const mongoose = require('mongoose');
+const request = require('request');
 let recipe = mongoose.model('Recipe');
 let user = mongoose.model('User');
+const http = require('http');
 
 let saveRecipe = (req, res, next) => {
-    let Recipe = new recipe;
-    Recipe.user = req.body.user;
-    Recipe.title = req.body.title;
-    Recipe.instructions = req.body.instructions;
-    Recipe.category = req.body.category;
-    Recipe.cuisine = req.body.cuisine;
-    //Recipe.ingredients = req.body.ingredients; <== FIX me :)
-    Recipe.imgURL = req.body.imgURL;
-    Recipe.save((err, savedObj) => {
-        if (err) {
-            console.log(err);
-            res.status(403).send({ error: "error saving to database" });
-            return;
+    user.findById({_id: req.body.id},(err, doc) => {
+        if(err){
+            res.status(500).send("Error reading database").end();
+        }
+        if(!doc){
+            res.status(404).send("user not found");
         }
         else {
-            //get ID of document we just created...add it to requesting
-            //users model in mongoDB: 
-            objref = savedObj.id;
-            user.findOneAndUpdate(
-                { email: Recipe.user },
-                { $push: { "recipesList": objref } }, (err) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(403).send({error: "error saving to user"});
-                        return;
-                    }
-                })
-            res.status(200);
-            return;
+            doc.recipesList.push({
+                title:req.body.title,
+                instructions:req.body.instructions,
+                category:req.body.category,
+                imgURL:req.body.imgURL,
+                cuisine:req.body.cuisine,
+
+            })
+            doc.markModified('recipesList');
+            doc.save();
+            res.status(200).json(doc);
         }
     })
 }
 
+let getSavedRecipes = (req, res, next) => {
+    user.findById({ _id: req.query.id }, (err, doc) => {
+        if (err) {
+            console.log(err);
+            res.status(403).send({ error: "Failed to find user..." });
+        }
+        //doc.recipesList is list of recipeID's saved for requesting User:
+        //console.log(callback);
+        res.status(200).json(doc);
+       
+    });
+}
+
+//moved to frontend: RecipeAPI service: 
+let getRandomRecipe = (req, res, next) => {
+    request.get('')
+        .on('response', (response) => {
+            console.log(response);
+            //return response; 
+        }).pipe(response);
+    res.status(200);
+    return;
+}
+
+let getRecipeByID = () => {
+
+}
+
 module.exports = {
     saveRecipe,
+    getSavedRecipes,
+    getRandomRecipe,
+
 }
