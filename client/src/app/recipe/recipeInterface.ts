1c5e74d82e929652b7332f6218473968f82ada55
Walker Chesley <chesley.walker@gmail.com>
2019-12-07 20:40:27 -0600

    added recipe interface

---
+++ client/src/app/recipe/recipeInterface.ts
@@ -0,0 +1,9 @@
+export interface Recipes {
+    recipetitle: string,
+    Img: string,
+    Instructions: string,
+    Cuisine: string,
+    Category: string,
+    //Ingredients:[String],
+    //Measurements:[String]
+};
